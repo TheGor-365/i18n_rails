73288f4024d8294f802899f8bb38691893133b40
Gor <7495500@gmail.com>
2023-06-02 13:12:38 +0300

    Add users resources and wellcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'wellcome/index'
+  root "wellcome#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
 end
